c2ce532a87777a1909c18076e27f3f8cbc9bc219
Camilo Vargas <camilovargasingm@gmail.com>
2023-10-26 21:57:52 -0500

    feat: add links in the nav

--- src/Layout/Home.tsx
+++ src/Layout/Home.tsx
@@ -1,19 +1,43 @@
+import { buttonVariants } from "@/components/ui/Button";
+import { cn } from "@/lib/utils";
+import { Link, Outlet } from "react-router-dom";
 
+const navItems = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "login",
+    path: "/login",
+  },
+  {
+    name: "register",
+    path: "/register",
+  },
+  {
+    name: "list of users",
+    path: "/users",
+  },
+]
 export default function Home() {
   return (
-    <div className="grid grid-cols-[300px,1fr]">
-      <aside className="h-screen bg-muted p-8">
+    <div className="grid grid-cols-[min-content,1fr] md:grid-cols-[250px,1fr]">
+      <aside className="h-screen bg-muted p-8 px-5 md:px-8">
         <nav>
           <ul>
-            <li>home</li>
-            <li>login</li>
-            <li>register</li>
-            <li>list of users</li>
+            {
+              navItems.map(item => (
+                <li key={item.path}>
+                  <Link to={item.path} className={cn(buttonVariants({ variant: "link" }), "w-full")}>{item.name}</Link>
+                </li>
+              ))
+            }
           </ul>
         </nav>
       </aside>
       <main className="p-8">
-        working
+        <Outlet />
       </main>
     </div>
   )
