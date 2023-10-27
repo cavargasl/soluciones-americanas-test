fdda08eabd61a68d932280a127c8b549e03d1951
Camilo Vargas <camilovargasingm@gmail.com>
2023-10-26 22:59:30 -0500

    feat: setup axios

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { AuthProvider } from "./provider/AuthProvider"
 import Router from "./routers"
-
+import './config/axios.config'
 
 const queryClient = new QueryClient({
   defaultOptions: {
