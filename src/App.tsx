f4052dabbdb4b758fa5cdc4dc5d5c8d41a95709b
Camilo Vargas <camilovargasingm@gmail.com>
2023-10-26 22:37:10 -0500

    feat: setup new dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { AuthProvider } from "./provider/AuthProvider"
 import Router from "./routers"
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5
+    }
+  }
+})
 
 function App() {
 
   return (
-    <AuthProvider>
-      <Router />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Router />
+      </AuthProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
