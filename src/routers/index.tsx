8d77299b73b74194f5ec8d22b620785670fa8aa2
Camilo Vargas <camilovargasingm@gmail.com>
2023-10-26 22:00:26 -0500

    feat: implement basic view of each

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -8,7 +8,10 @@ export default function Router() {
       <Routes>
 
         <Route path="/" element={<Home />} >
-          
+          <Route index element={<h1>Home view</h1>} />
+          <Route path="login" element={<h1>Login view</h1>} />
+          <Route path="register" element={<h1>Register view</h1>} />
+          <Route path="users" element={<h1>Users view</h1>} />
         </Route>
 
       </Routes>
